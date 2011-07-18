ee33c7db96e44ea504c2b532b213e411e398dbe1
HuangQiyuan <122253922@qq.com>
2011-07-18 11:57:41 +0800

    Preliminary build applications(Commodities management), Close gh-1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'cc97f73ba7c286271cc55cf526b9de0768e2e761d237be420cb0aefa32be9a7086b7b3e1c5ee9a762917623c95db7ab71b5cdf2bfa7d7740173c9a4795a0af51'
