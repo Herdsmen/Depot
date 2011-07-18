8a8afba7f2162c92d174db8e076ebe81af222a2d
HuangQiyuan <122253922@qq.com>
2011-07-18 14:43:32 +0800

    Shopping cart, Close gh-4

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 # 
