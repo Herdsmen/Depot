aa693d2c27a5c94d3da43a24eba5dfbbacb1278b
NgSengfok <1054764294@qq.com>
2011-07-20 13:46:59 +0800

    Closing account, Close gh-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Depot::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get "store/index"
 
-  resources :products
+  resources :products do
+	get :who_bought, :on => :member
+  end
+  
+  # resources :products
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
