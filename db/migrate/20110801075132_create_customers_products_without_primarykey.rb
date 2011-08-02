96757fa75ea5109dd002be4825f64d51ff3f4953
chmingl <chmingl@qq.om>
2011-08-02 14:32:52 +0800

    add role:customer,force create table customers_products

--- db/migrate/20110801075132_create_customers_products_without_primarykey.rb
+++ db/migrate/20110801075132_create_customers_products_without_primarykey.rb
@@ -1,6 +1,6 @@
 class CreateCustomersProductsWithoutPrimarykey < ActiveRecord::Migration
   def self.up
-    create_table :customers_products,:id=>false do |t|
+    create_table :customers_products,:id=>false ,:force => true do |t|
       t.integer :customer_id, :null => false
       t.integer :product_id, :null => false
     end
