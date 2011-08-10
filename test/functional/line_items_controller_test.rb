75edbab0b6bc6cc0f1127ef03f2573a0970b5a84
chmingl <chmingl@qq.om>
2011-08-10 10:24:12 +0800

    add unit test and functional test

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -6,12 +6,14 @@ class LineItemsControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
+    user_login :one
     get :index
     assert_response :success
     assert_not_nil assigns(:line_items)
   end
 
   test "should get new" do
+    user_login :one
     get :new
     assert_response :success
   end
@@ -34,21 +36,25 @@ class LineItemsControllerTest < ActionController::TestCase
 	end
 
   test "should show line_item" do
+    user_login :one
     get :show, :id => @line_item.to_param
     assert_response :success
   end
 
   test "should get edit" do
+    user_login :one
     get :edit, :id => @line_item.to_param
     assert_response :success
   end
 
   test "should update line_item" do
+    user_login :one
     put :update, :id => @line_item.to_param, :line_item => @line_item.attributes
     assert_redirected_to line_item_path(assigns(:line_item))
   end
 
   test "should destroy line_item" do
+    user_login :one
     assert_difference('LineItem.count', -1) do
       delete :destroy, :id => @line_item.to_param
     end
